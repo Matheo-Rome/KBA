8a91bed20aab1c79e54ab9f9de5ed5953ee6b669
ange <79404132+anglnn@users.noreply.github.com>
2023-07-03 17:47:14 +0200

    forest scene

--- Assets/Script/Combat/Target.cs
+++ Assets/Script/Combat/Target.cs
@@ -4,23 +4,53 @@ public class Target : MonoBehaviour
 {
     public float health = 30f;
     public float corruption = 50f;
+    Renderer ren;
+    public GameObject bunny;
+
+    void Start()
+    {
+        ren = GetComponent<Renderer>();
+        ren.material.color = Color.black;
+    }
 
     public void TakeDamage(float amount)
     {
         health -= amount;
-        if (health <= 0f)
+        switch (health)
         {
-            Die();
+            case <= 0f:
+                Die();
+                break;
+            case <= 10f:
+                ren.material.color = Color.magenta;
+                break;
+            case <= 20f:
+                ren.material.color = Color.red;
+                break;    
+            default:
+                ren.material.color = Color.black;
+                break;
         }
     }
 
     public void TakeHealing(float amount)
     {
         corruption -= amount;
-        if (corruption <= 0f)
+        switch (corruption)
         {
-            Debug.Log("Saved !");
-            Die();
+            case <= 0f:
+                Die();
+                Instantiate(bunny, transform.position, transform.rotation);
+                break;
+            case <= 10f:
+                ren.material.color = Color.cyan;
+                break;
+            case <= 20f:
+                ren.material.color = Color.blue;
+                break;
+            default:
+                ren.material.color = Color.black;
+                break;
         }
     }
 
---
+++ Assets/Script/Controllers/Wander.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Wander : MonoBehaviour
+{
+	public float duration;    //the max time of a walking session (set to ten)
+    float elapsedTime   = 0f; //time since started walk
+    float wait          = 0f; //wait this much time
+    float waitTime      = 0f; //waited this much time
+    public float speed = 1f;
+
+    float randomX;  //randomly go this X direction
+    float randomZ;  //randomly go this Z direction
+
+    bool move = true; //start moving
+
+    Vector3 pos;
+
+    void Start(){
+        randomX =  Random.Range(-3,3);
+        randomZ = Random.Range(-3,3);
+    }
+
+    void Update ()
+    {
+        if (elapsedTime < duration && move) 
+        {
+            //if its moving and didn't move too much
+            Vector3 movement = new Vector3(randomX,0,randomZ);
+            transform.Translate (movement * Time.deltaTime * speed);
+            transform.rotation = Quaternion.LookRotation(movement, Vector3.up);         
+    
+            elapsedTime += Time.deltaTime;
+        } 
+        else if (move)
+        {
+            //do not move and start waiting for random time
+            move        = false;
+            wait        = Random.Range (5, 10);
+            waitTime    = 0f;
+        }
+
+        if (waitTime < wait && !move) 
+        {
+            //you are waiting
+            waitTime += Time.deltaTime;
+        } 
+        else if(!move)
+        {
+            move = true;
+            elapsedTime = 0f;
+            randomX = Random.Range(-3,3);
+            randomZ = Random.Range(-3,3);
+        }
+        pos = transform.position;
+    }
+}
